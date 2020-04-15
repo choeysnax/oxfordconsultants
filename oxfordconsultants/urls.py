d517b35eeb0e70ce55067e46817e31b65d41281f
Charles Thompson <cpkthompson@gmail.com>
2020-04-15 14:36:10 +0000

    Insight model

--- oxfordconsultants/urls.py
+++ oxfordconsultants/urls.py
@@ -3,13 +3,12 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.urls import path
 from wagtail.admin import urls as wagtailadmin_urls
-from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
 from search import views as search_views
 
 urlpatterns = [
-    url(r'^django-admin/', admin.site.urls),
+    url(r'^backend/', admin.site.urls),
 
     url(r'^admin/', include(wagtailadmin_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
