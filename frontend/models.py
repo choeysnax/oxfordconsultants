9981c1b2f52e050fc48f77fb561bd7510332b811
Charles Thompson <cpkthompson@gmail.com>
2020-07-07 20:57:57 +0000

    registration pages

--- frontend/models.py
+++ frontend/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
+from modelcluster.fields import ParentalKey
+from modelcluster.models import ClusterableModel
+from wagtail.admin.edit_handlers import MultiFieldPanel, InlinePanel, FieldPanel
+from wagtail.core.fields import RichTextField
+from wagtail.core.models import Page, Orderable
 
-# Create your models here.
+
+class HomePage(Page):
+    pass
 
 
 class Insight(models.Model):
@@ -25,3 +32,25 @@ class Testimonial(models.Model):
 
     def __str__(self):
         return self.author
+
+
+class Section(Orderable):
+    title = models.CharField(max_length=140, blank=True)
+    body = RichTextField(max_length=255, null=True, blank=True)
+    page = ParentalKey('RegistrationPage', on_delete=models.CASCADE, related_name='sections')
+    panels = [
+        FieldPanel('title'),
+        FieldPanel('body'),
+    ]
+
+
+class RegistrationPage(Page, ClusterableModel):
+    introduction_text = RichTextField(blank=True, null=True)
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel([
+            FieldPanel('introduction_text'),
+            InlinePanel('sections', label="Section"),
+
+        ], heading="Sections")
+    ]
--- oxfordconsultants/urls.py
+++ oxfordconsultants/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import path
 from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
 from oxfordconsultants.sitemap import StaticViewSitemap
@@ -16,7 +17,7 @@ sitemaps = {
 urlpatterns = [
     url(r'^backend/', admin.site.urls),
 
-    url(r'^admin/', include(wagtailadmin_urls)),
+    url(r'^wagtail/', include(wagtailadmin_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
 
     url(r'^search/$', search_views.search, name='search'),
@@ -25,11 +26,11 @@ urlpatterns = [
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
     path('', include('frontend.urls')),
-    # url(r'', include(wagtail_urls)),
+    url(r'', include(wagtail_urls)),
 
     # Alternatively, if you want Wagtail pages to be served from a subpath
     # of your site, rather than the site root:
-    #    url(r'^pages/', include(wagtail_urls)),
+    # url(r'^pages/', include(wagtail_urls)),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
          name='django.contrib.sitemaps.views.sitemap')
 ]
